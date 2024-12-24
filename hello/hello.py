d624fb637ab230609bb70e622d8c0517611c5b97
Yehan2003 <Yehan2003Kariyawasam@gmail.com>
2024-12-24 13:38:41 +0530

    Add files via upload

---
+++ hello/hello.py
@@ -0,0 +1,11 @@
+
+# Simple program to calculate the side length of a square
+
+
+area = float(input("Enter the area of the square: "))
+
+
+side_length = area ** 0.5
+
+
+print(f"The side length of the square is: {side_length}")
